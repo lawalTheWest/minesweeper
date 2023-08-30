1417664397fe09cd3ab23e50e5577725f54f3236
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2023-08-30 18:50:25 +0100

    wrote and readjusted the Cell class and also started the board class. the attributes are updated likewise the methods

--- minesweeper_classes.py
+++ minesweeper_classes.py
@@ -1,8 +1,36 @@
 #!/usr/bin/python3
 
+import random
+
+
+class Cell:
+    def __init__(self):
+        self.is_mine = False
+        # Indicates whether the cell contains a mine.
+
+        self.is_flagged = False
+        # Indicates whether the cell has been flagged by the player.
+
+        self.is_revealed = False
+        # Indicates whether the cell has been revealed.
+
+        self.adjacent_mines = 0
+        # Number of adjacent cells containing mines.
+
+    def reveal(self):
+        # Reveals the content of the cell.
+        self.is_revealed = True
+
+    def toggle_flag(self):
+        # Toggles the flagged state of the cell.
+        self.is_flagged = not self.is_flagged
+
+    def set_adjacent_mines(self, counter):
+        # Calculates and sets the number of adjacent mines.
+        self.adjacent_mines = counter
 
 class Board:
-    def __init__(self, num_rows, num_cols, num_mines, cells):
+    def __init__(self, num_rows, num_cols, num_mines):
         self.num_rows = num_rows
         # Number of rows on the board.
         
@@ -12,14 +40,34 @@ class Board:
         self.num_mines = num_mines
         # Number of mines on the board.
 
-        self.cells = cells
+        '''
+            progressive work
+            Create a nested list with column and cell number.
+            It iterates col number times and 
+            creates a new Cell object for each iteration.
+            The use of an underscore _ as the variable name
+            indicates that the variable is not going to
+            be used within the loop.
+        '''
+
+        self.cells = [[cells() for _ in range(num_cols)] for _ in range(num_rows)]
         # A 2D list of Cell objects representing the game grid.
 
     def initialize_board(self):
         # Initializes the board with empty cells.
+        self.place_mines()
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                self.set_adjacent_mines(row, col)
 
     def place_mines(self):
         # Randomly places mines on the board.
+        # create a range from 0 to the last cell number
+        # select randomly from our range
+        # passing the number of mines
+        pos_of_mine = random.sample(range(self.num_rows * self.num_cols), self.num_mines)
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
 
     def reveal_cell(self):
         # Reveals a cell and handles adjacent empty cells.
@@ -29,29 +77,6 @@ class Board:
 
     def is_game_over(self):
         # Checks if the game is over (win or loss).
-
-class Cell:
-    def __init__(self, is_mine, is_flagged, is_revealed, adjacent_mines):
-        self.is_mine = is_mine
-        # Indicates whether the cell contains a mine.
-        
-        self.is_flagged = is_flagged
-        # Indicates whether the cell has been flagged by the player.
-        
-        self.is_revealed = is_revealed
-        # Indicates whether the cell has been revealed.
-        
-        self.adjacent_mines = adjacent_mines
-        # Number of adjacent cells containing mines.
-
-    def reveal():
-        # Reveals the content of the cell.
-
-    def toggle_flag():
-        # Toggles the flagged state of the cell.
-
-    def set_adjacent_mines():
-        # Calculates and sets the number of adjacent mines.
 
 class Game:
     def __init__(self, board, game_over, win):
